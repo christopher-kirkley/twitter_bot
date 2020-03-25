14a21bb56015195d031ba31f182608fd64c5190c
Christopher Kirkley <ckirkley@gmail.com>
2020-03-25 10:01:20 -0700

    Changed interval.

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,19 +1,18 @@
 import tweepy
 from config import *
 import time
+from random import randint
 
 from read import get_tweet
-
-INTERVAL = 60
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
-
-
 while True:
+    minutes = randint(1, 3600)
+    INTERVAL = 60 * minutes
     print("Tweeting.")
     tweet = get_tweet()
     api.update_status(tweet)
